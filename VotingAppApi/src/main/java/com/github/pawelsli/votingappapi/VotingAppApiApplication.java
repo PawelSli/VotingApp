39039dd12a506bf86e9de345dfbe6916e7f39811
Paweł Śliwa <pawel.sliwa@medcoreproject.pl>
2024-09-28 15:05:21 +0200

    Initializing Angular and Spring Boot projects

---
+++ VotingAppApi/src/main/java/com/github/pawelsli/votingappapi/VotingAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.pawelsli.votingappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingAppApiApplication.class, args);
+    }
+
+}
---
+++ VotingAppApi/src/test/java/com/github/pawelsli/votingappapi/VotingAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pawelsli.votingappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
